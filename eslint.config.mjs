5a7ac0d42be776cf058ec9f058bd8e20a76f8741
Julio Oliveira <julioc@weg.net>
2026-08-09 13:16:04 -0300

    feat: add tanstack-query support and define project architecture and business requirements

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,15 @@
 import nextPlugin from '@next/eslint-plugin-next'
 import tsPlugin from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 
 export default [
     {
         files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
         plugins: {
             '@next/next': nextPlugin,
-            '@typescript-eslint': tsPlugin
+            '@typescript-eslint': tsPlugin,
+            '@tanstack/query': pluginQuery
         },
         languageOptions: {
             parser: tsParser,
@@ -22,7 +24,8 @@ export default [
         rules: {
             ...nextPlugin.configs.recommended.rules,
             ...nextPlugin.configs['core-web-vitals'].rules,
-            ...tsPlugin.configs.recommended.rules
+            ...tsPlugin.configs.recommended.rules,
+            '@tanstack/query/exhaustive-deps': 'error'
         }
     },
     {
